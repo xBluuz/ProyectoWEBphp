a68c32db3dbd1d035f9107c1d504e535149b4dc6
xBluuz <132654466+xBluuz@users.noreply.github.com>
2024-01-27 13:26:10 +0100

    ac

--- PanelAdmin/PanelAdmin.php
+++ PanelAdmin/PanelAdmin.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styleA.css">
+    <link rel="shortcut icon" href="./Img/logo_salesianos.png">
     <title>Document</title>
     <?php 
     include("../conexion_bd.php");
--- PanelUser/PanelUser.php
+++ PanelUser/PanelUser.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styleU.css">
+    <link rel="shortcut icon" href="./Img/logo_salesianos.png">
     <title>Document</title>
     <?php 
     session_start();
