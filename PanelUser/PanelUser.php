b249c7d0c623512f9210ed7c48bcb439848230b0
4l3j4ndr0P <alejandropalazon@outlook.es>
2024-01-28 19:33:13 +0100

    CSS 3

--- PanelUser/PanelUser.php
+++ PanelUser/PanelUser.php
@@ -15,10 +15,65 @@
         header('Location: ../Login.php');
     }
     ?>
+    <script>
+        var estado_nav= 0;
+        const busqueda = window.location.href.split("?")[0];
+        function enviarValor(event){
+            let newUrl = busqueda + "?search=" + event.target.value
+            window.location.href = newUrl
+        }
+        function activarAnimacion() {
+            var estado_nav = obtenerEstadoNav();
+
+            if (estado_nav === "0") {
+                var filtros = document.querySelector('.filter_var');
+                filtros.classList.toggle('animado');
+                estado_nav = "1";
+                console.log(estado_nav);
+                document.getElementById('boton_filtro').innerHTML = "<";
+            } else {
+                var filtros = document.querySelector('.filter_var');
+                filtros.classList.remove('animado');
+                estado_nav = "0";
+                console.log(estado_nav);
+                document.getElementById('boton_filtro').innerHTML = ">";
+            }
+            guardarEstadoNav(estado_nav);
+        }
+        function activarAnimacionRefresh() {
+            var estado_nav = obtenerEstadoNav();
+
+            if (estado_nav === "1") {
+                var filtros = document.querySelector('.filter_var');
+                filtros.classList.toggle('animado');
+                document.getElementById('boton_filtro').innerHTML = "<";
+            }
+            if (estado_nav === "0") {
+                var filtros = document.querySelector('.filter_var');
+                filtros.classList.remove('animado');
+                document.getElementById('boton_filtro').innerHTML = ">";
+            }
+            guardarEstadoNav(estado_nav);
+        }
+        function guardarEstadoNav(estado_nav) {
+            document.cookie = "estado_nav=" + estado_nav + "; expires=Fri, 31 Dec 9999 23:59:59 GMT"+"path=/";
+        }
+        function obtenerEstadoNav() {
+            var nombre = "estado_nav=";
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = cookies[i].trim();
+                if (cookie.indexOf(nombre) === 0) {
+                    return cookie.substring(nombre.length, cookie.length);
+                }
+            }
+            return "";
+        }
+    </script>
 
 </head>
 
-<body>
+<body onload='activarAnimacionRefresh()'>
     <header>
         <div class="texto1">
             <strong>Panel Usuario de
@@ -30,7 +85,30 @@
         </div>
     </header>
     <main>
-        <div class="">
+        <div class="filter_var">
+            <div class="filtros">
+                <div class="div_filtro_principal">
+                    <h2>Ordenar por:</h2>
+                    <select name="filtro_principal" id="filtro_principal" class="filtro_principal" onchange="enviarValor(event)" required>
+                        <option value="">Todo</option>
+                    </select>
+                </div>
+                <div class="div_filtro_secundario">
+                    <h2>Seleccionar:</h2>
+                    <select name="filtro_secundario" id="filtro_secundario" class="filtro_secundario">
+                        <option value="">Todo</option>
+                    </select>
+                </div>
+                <div class="div_boton_crear">
+                    <h2>Añadir Inventario:</h2>
+                    <button class="boton_crear" name="boton_crear" id="boton_crear">+</button>
+                </div>
+            </div>
+            <div class="div_boton_filtro">
+                <button class="boton_filtro" id="boton_filtro" name="boton_filtro" onclick="activarAnimacion()">></button>
+            </div>
+        </div>
+        <div class="principal">
             <?php include("./InformacionU.php") ?>
         </div>
     </main>
