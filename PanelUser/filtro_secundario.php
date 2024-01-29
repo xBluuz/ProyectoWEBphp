ec0bdcd92d3c4d4c46b9b2a54a914a7cd9463fc8
xBluuz <132654466+xBluuz@users.noreply.github.com>
2024-01-29 14:46:52 +0100

    Revert "Merge branch 'main' of https://github.com/xBluuz/ProyectoWEBphp"
    This reverts commit c3223043925996d8c63dc7731ea6b8dc7be56192, reversing
    changes made to 6ac10125a9aa81f5cac80bb46e03c09ca0c6b979.

--- PanelUser/filtro_secundario.php
+++ PanelUser/filtro_secundario.php
@@ -1,13 +1,12 @@
 <?php
-if (isset($_GET['search']) && !empty($_GET['search']) ||  (isset($_GET['value']) && !empty($_GET['value']))) {
+if (isset($_GET['search']) && !empty($_GET['search'])) {
     $sql = "SELECT * FROM $_GET[search] WHERE (idDepartamento =  $_SESSION[idDepartamento] OR idDepartamento = 0)";
     $result = mysqli_query($conexion, $sql);
     while ($row = mysqli_fetch_array($result)) {
         $key = array_keys($row);
         $all = $row[$key[2]];
-
-       echo $all == $_GET['value'] ? "<option value='$all' selected>$all</option>" : "<option value='$all'>$all</option>";
-  
+        echo "
+        <option value='$all'>$all</option>";
     }
 }
 ?>
