7c480ce5a6c7eda586f2d42ce68741b54aefe2fb
Juan Antonio <xbluuz@gmail.com>
2024-01-30 08:25:33 +0100

    ac

--- PanelAdmin/PanelAdmin.php
+++ PanelAdmin/PanelAdmin.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styleA.css">
-    <link rel="shortcut icon" href="./Img/logo_salesianos.png">
-    <title>Document</title>
+    <link rel="shortcut icon" href="../Img/logo_salesianos.png">
+    <title>Almacen</title>
     <?php 
     include("../conexion_bd.php");
     session_start();
--- PanelUser/PanelUser.php
+++ PanelUser/PanelUser.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styleU.css">
-    <link rel="shortcut icon" href="./Img/logo_salesianos.png">
-    <title>Document</title>
+    <link rel="shortcut icon" href="../Img/logo_salesianos.png">
+    <title>Almacen</title>
     <?php
     session_start();
     if ($_SESSION['login']) {
