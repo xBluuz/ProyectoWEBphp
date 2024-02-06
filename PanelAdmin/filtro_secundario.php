2208dff538374cfd6f662d83bcdd99932694bc39
el12pachi <javiervicente20laalmunia@gmail.com>
2024-02-06 17:03:28 +0100

    Fix the bug

--- PanelAdmin/filtro_secundario.php
+++ PanelAdmin/filtro_secundario.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_GET['search']) && !empty($_GET['search']) ||  (isset($_GET['value']) && !empty($_GET['value']))) {
-    $sql = "SELECT * FROM $_GET[search] WHERE (idDepartamento =  $_SESSION[idDepartamento] OR idDepartamento = 0)";
+    $sql = "SELECT * FROM $_GET[search]";
     $result = mysqli_query($conexion, $sql);
     while ($row = mysqli_fetch_array($result)) {
         $key = array_keys($row);
