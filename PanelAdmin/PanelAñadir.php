f6c0324006ccb0d2df7cb0c8136a0276db7d3d6d
4l3j4ndr0P <alejandropalazon@outlook.es>
2024-02-12 08:30:58 +0100

    Solucion Boton Cerrar

--- PanelAdmin/PanelAñadir.php
+++ PanelAdmin/PanelAñadir.php
@@ -57,8 +57,13 @@
             }
             ?>
         </select>
-        <div class="submit_container">
-            <input type="submit" value="Send" name="send" class="submit_add">
+        <div class="button_container">
+            <div class="cerrar_container">
+                <button class="cerrar_add_button" onclick="cerrarAñadir()" id="cerrar_add_button">Cerrar</button>
+            </div>
+            <div class="submit_container">
+                <input type="submit" value="Send" name="send" class="submit_add">
+            </div>
         </div>
     </form>
 </div>
