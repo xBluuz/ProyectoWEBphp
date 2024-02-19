cf60496f6ef592223c8a52e69c8781b3792763b1
Juan Antonio <xbluuz@gmail.com>
2024-02-19 11:55:02 +0100

    ac

--- PanelAdmin/InformaAula.php
+++ PanelAdmin/InformaAula.php
@@ -1,12 +1,9 @@
 <?php 
 include("./conexion_bd.php");
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['AAñadir'])){
-    $sql = "INSERT INTO aula (idUbicacion, NombreAula, idDepartamento) VALUES ($_POST[AUbi], '$_POST[AAula]', $_POST[ADepartamento])";
-    echo $sql;
+    $sql = "INSERT INTO aula (idUbicacion, idAula, idDepartamento) VALUES ($_POST[AUbi], '$_POST[AAula]', $_POST[ADepartamento])";
+    // echo $sql;
     $conexion->query($sql);
     }
-    header("location:PanelAdmin.php");
-}
+header("location:PanelAdmin.php");
 ?>
